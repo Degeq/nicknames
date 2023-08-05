845c4f246a12eb13fd5d896a814caff97346ed45
Degeq <gorlovoy89@mail.ru>
2023-08-05 13:16:50 +0300

    DelivererHW Commit 2

--- Deliverer/src/main/java/Main.java
+++ Deliverer/src/main/java/Main.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -10,10 +9,47 @@ public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        Thread thread = new Thread(() -> {
+            int maxValue = 0;
+            int maxKey = 0;
+            int f = 0;
+            synchronized (sizeToFreq) {
+                while (!Thread.interrupted()) {
+                    if (sizeToFreq.isEmpty()) {
+                        try {
+                            sizeToFreq.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        try {
+                            sizeToFreq.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            e.printStackTrace();
+                        }
+                        f++;
+                        for (Map.Entry<Integer, Integer> i : sizeToFreq.entrySet()) {
+                            if (i.getValue() > maxValue) {
+                                maxKey = i.getKey();
+                                maxValue = i.getValue();
+                            }
+                        }
+                        System.out.println("Самое частое кол-во R : " + maxKey + " - " + maxValue + " раз в " + f + " итерации");
+
+                    }
+                }
+            }
+        });
+
+        thread.start();
+
         ExecutorService threadPool = Executors.newFixedThreadPool(threadsCount);
         List<Future> futures = new ArrayList<>();
         for (int i = 0; i < threadsCount - 1; i++) {
             futures.add(threadPool.submit(() -> {
+
                 String route = generateRoute("RLRFR", 100);
                 int rCalc = 0;
                 for (int j = 0; j < route.length(); j++) {
@@ -23,6 +59,7 @@ public class Main {
                     }
                 }
                 synchronized (sizeToFreq) {
+                    sizeToFreq.notify();
                     if (sizeToFreq.containsKey(rCalc)) {
                         sizeToFreq.replace(rCalc, sizeToFreq.get(rCalc) + 1);
                     } else {
@@ -33,14 +70,17 @@ public class Main {
             }));
         }
 
-        for (int i = 0; i < threadsCount - 1; i++) {
-            futures.get(i).get();
+
+        for (Future i : futures) {
+            i.get();
         }
+
+
+        thread.interrupt();
         threadPool.shutdown();
 
         int maxValue = 0;
         int maxKey = 0;
-        System.out.println("Количество R, встречаемое в маршрутах повторялось со следующей частотой: ");
         for (Map.Entry<Integer, Integer> i : sizeToFreq.entrySet()) {
             if (i.getValue() > maxValue) {
                 maxKey = i.getKey();
@@ -49,6 +89,7 @@ public class Main {
             System.out.println(i.getKey() + " R - " + i.getValue() + " раз");
         }
         System.out.println("Самое частое кол-во R : " + maxKey + " - " + maxValue + " раз");
+
     }
 
     public static String generateRoute(String letters, int length) {
